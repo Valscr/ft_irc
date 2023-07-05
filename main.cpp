8964829008d2d1d7f94bf0d4b4ede2b87ce155b2
Valscr <95771970+Valscr@users.noreply.github.com>
2023-07-05 13:27:35 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <poll.h>
+
+#define MAX_CLIENTS 10
+#define BUFFER_SIZE 1024
+
+int main() {
+    int listenSocket, clientSocket;
+    struct sockaddr_in serverAddress;
+    std::vector<pollfd> fds(MAX_CLIENTS + 1); // +1 pour le socket d'écoute
+
+    // Création du socket d'écoute
+    listenSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (listenSocket < 0) {
+        perror("Erreur lors de la création du socket d'écoute");
+        exit(1);
+    }
+
+    // Configuration de l'adresse du serveur
+    memset(&serverAddress, 0, sizeof(serverAddress));
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    serverAddress.sin_port = htons(8080); // Port d'écoute
+
+    // Lier le socket d'écoute à l'adresse et au port spécifiés
+    if (bind(listenSocket, reinterpret_cast<struct sockaddr*>(&serverAddress), sizeof(serverAddress)) < 0) {
+        perror("Erreur lors de la liaison du socket d'écoute");
+        exit(1);
+    }
+
+    // Mettre le socket d'écoute en mode écoute
+    if (listen(listenSocket, MAX_CLIENTS) < 0) {
+        perror("Erreur lors de la mise en écoute du socket");
+        exit(1);
+    }
+
+    // Mettre le socket d'écoute en mode non-bloquant
+    if (fcntl(listenSocket, F_SETFL, O_NONBLOCK) < 0) {
+        perror("Erreur lors de la configuration du socket d'écoute en mode non-bloquant");
+        exit(1);
+    }
+    // Ajouter le socket d'écoute à la structure pollfd
+    for (int j = 0; j <= MAX_CLIENTS; j++)
+    {
+        fds[j].fd = listenSocket;
+        fds[j].events = POLLIN;
+    }
+    // Boucle principale pour attendre les événements
+    while (true) {
+        int result = poll(fds.data(), fds.size(), -1); // Attendre indéfiniment
+        if (result < 0) {
+            perror("Erreur lors de l'appel à poll");
+            exit(1);
+        }
+
+        // Vérifier les événements sur les descripteurs de fichiers surveillés
+        for (size_t i = 0; i < fds.size(); i++) {
+            if (fds[i].revents & POLLIN) {
+                if (fds[i].fd == listenSocket) {
+                    // Événement sur le socket d'écoute (nouvelle connexion entrante)
+                    clientSocket = accept(listenSocket, nullptr, nullptr);
+                    if (clientSocket < 0) {
+                        if (errno != EWOULDBLOCK && errno != EAGAIN) {
+                            perror("Erreur lors de l'acceptation de la connexion");
+                            exit(1);
+                        }
+                    } else {
+                        // Mettre le socket client en mode non-bloquant
+                        if (fcntl(clientSocket, F_SETFL, O_NONBLOCK) < 0) {
+                            perror("Erreur lors de la configuration du socket client en mode non-bloquant");
+                            exit(1);
+                        }
+
+                        // Ajouter le socket client à la structure pollfd
+                        fds[i].fd = clientSocket;
+                        fds[i].events = POLLIN;
+
+                        std::cout << "Cient " << i  << " connecté" << std::endl;
+                    }
+                } else {
+                    // Événement sur un socket client existant (données reçues)
+                    char buffer[BUFFER_SIZE];
+                    memset(buffer, 0, sizeof(buffer));
+
+                    int bytesRead = recv(fds[i].fd, buffer, sizeof(buffer), 0);
+                    if (bytesRead < 0) {
+                        if (errno != EWOULDBLOCK && errno != EAGAIN) {
+                            perror("Erreur lors de la lecture des données");
+                            exit(1);
+                        }
+                    } else if (bytesRead == 0) {
+                        // Connexion fermée par le client
+                        close(fds[i].fd);
+                        fds[i].fd = -1;
+
+                        std::cout << "Connexion fermée client " << i << std::endl;
+                    } else {
+                        // Traiter les données reçues
+                        std::cout << "Client " << i << " : " << buffer << std::endl;
+                    }
+                }
+            }
+        }
+    }
+
+    return 0;
+}
