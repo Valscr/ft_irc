d6d847c90dd06bc9bd1cf10b8c990c6ce72357f1
Valscr <95771970+Valscr@users.noreply.github.com>
2023-07-06 06:08:47 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <list>
 #include <cstring>
 #include <unistd.h>
 #include <fcntl.h>
@@ -15,6 +16,7 @@ int main(int argc, char **argv) {
         return (0);
     std::string pass = argv[2];
     int port = std::atoi(argv[1]);
+    int password[MAX_CLIENTS];
     int listenSocket, clientSocket;
     struct sockaddr_in serverAddress;
     std::vector<pollfd> fds(MAX_CLIENTS + 1); // +1 pour le socket d'écoute
@@ -53,7 +55,8 @@ int main(int argc, char **argv) {
     for (int j = 0; j <= MAX_CLIENTS; j++)
     {
         fds[j].fd = listenSocket;
-        fds[j].events = POLLIN;
+        fds[j].events = POLLIN | POLLOUT;
+        password[j] = 0;
     }
     // Boucle principale pour attendre les événements
     while (true) {
@@ -65,7 +68,7 @@ int main(int argc, char **argv) {
 
         // Vérifier les événements sur les descripteurs de fichiers surveillés
         for (size_t i = 0; i < fds.size(); i++) {
-            if (fds[i].revents & POLLIN) {
+            if (fds[i].revents) {
                 if (fds[i].fd == listenSocket) {
                     // Événement sur le socket d'écoute (nouvelle connexion entrante)
                     clientSocket = accept(listenSocket, nullptr, nullptr);
@@ -92,29 +95,40 @@ int main(int argc, char **argv) {
                     std::string buffer(BUFFER_SIZE, '\0');
 
                     int bytesRead = recv(fds[i].fd, &buffer[0], buffer.size(), 0);
-                    if (bytesRead < 0) {
+                    if (bytesRead < 0)
+                    {
                         if (errno != EWOULDBLOCK && errno != EAGAIN) {
                             perror("Erreur lors de la lecture des données");
                             exit(1);
                         }
-                    } else if (bytesRead == 0) {
+                    }
+                    else if (bytesRead == 0)
+                    {
                         // Connexion fermée par le client
                         close(fds[i].fd);
                         fds[i].fd = -1;
-
                         std::cout << "Connexion fermée client " << i << std::endl;
-                    } else {
+                    } 
+                    else if (password[i] == 0)
+                    {
                         if (pass == buffer.substr(buffer.find("PASS") + 5, pass.length()) && !std::isalpha(buffer[buffer.find("PASS") + 5 + pass.length()]))
                         {
                             std::cout << "Cient " << i  << " connecté" << std::endl;
-                            std::cout << "Client " << i << " : " << buffer << std::endl;
+                            send(fds[i].fd, "Bienvenu sur le serveur\n", 25, 0);
+                            std::cout << buffer << std::endl;
+                            password[i] = 1;
                         }
                         else
                         {
-                            send(fds[i].fd, ":serveur-irc 464 client :Mot de passe requis", 45, 0);
+                            //fds[i].events = POLLOUT;
+                            send(fds[i].fd, ":serveur-irc 464 client :Mot de passe requis\n", 46, 0);
                         }
                     }
-                }
+                    else
+                    {
+                        std::cout << "Client " << i << " : " << buffer << std::endl;
+                    }
+            }
             }
         }
     }
